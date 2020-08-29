17ed013ccdf62b4ad8ff3e4dcc5943ea8a73eb63
0xpFu <44911576+0xpFu@users.noreply.github.com>
2020-08-29 16:22:02 +0200

    Update calcul.rb

--- calcul.rb
+++ calcul.rb
@@ -1,7 +1,7 @@
-puts "Entre ton texte : "
+print "Entre ton texte : " # to get stdin beetwen text
  
 a = gets.chomp
  
 b = a.length
  
-puts "Ton texte comprends "+b.to_s+" caractères."
+puts "Ton texte comprends %s caractères." % b ## concatenate variable
